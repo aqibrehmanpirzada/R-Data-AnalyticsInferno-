57ddffd11fd4fba932aef4e3df3a204d6976d133
aqibrehmanpirzada <aqibrehmanpirzada75@gmail.com>
2023-12-11 19:24:48 +0500

    Second COmmit

---
+++ GUI app Demo in R Using Shinny/First App.r
@@ -0,0 +1,44 @@
+# Load R packages
+library(shiny)
+library(shinythemes)
+
+
+# Define UI
+ui <- fluidPage(theme = shinytheme("cerulean"),
+                navbarPage(
+                  # theme = "cerulean",  # <--- To use a theme, uncomment this
+                  "App in R Demo",
+                  tabPanel("Home",
+                           sidebarPanel(
+                             tags$h3("Enter Your Data:"),
+                             textInput("txt1", "Given Name:", ""),
+                             textInput("txt2", "Surname:", ""),
+                             
+                           ), # sidebarPanel
+                           mainPanel(
+                             h1("Header 1"),
+                             
+                             h4("Output 1"),
+                             verbatimTextOutput("txtout"),
+                             
+                           ) # mainPanel
+                           
+                  ), # Navbar 1, tabPanel
+                  tabPanel("Careers", "This panel is intentionally left blank"),
+                  tabPanel("About Us", "This panel is intentionally left blank")
+                  
+                ) # navbarPage
+) # fluidPage
+
+
+# Define server function  
+server <- function(input, output) {
+  
+  output$txtout <- renderText({
+    paste( input$txt1, input$txt2, sep = " " )
+  })
+} # server
+
+
+# Create Shiny object
+shinyApp(ui = ui, server = server)
